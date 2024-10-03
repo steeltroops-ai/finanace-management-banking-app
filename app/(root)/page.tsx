38fab08bf8f426b1fc4e57aa0e3c5ef096792309
Mayank Pratap Singh <87810082+steeltroops-ai@users.noreply.github.com>
2024-10-03 20:23:29 +0530

    ...

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: 'Mayank', lastName: 'Pratap Singh', emails: 'steeltroops.ai@gmail.com' };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and Manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1243.34}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.54 }, { currentBalance: 323.54 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
